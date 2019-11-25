78fd8b8676410b60303e8ea6472c7e7d8d8c57f1
刘恒 <ai747957633@qq.com>
2019-11-25 16:27:18 +0800

    refresh ignore

--- Config.php
+++ Config.php
@@ -8,9 +8,12 @@
 
 use okv3\Config;
 
+// 是否 debug
+Config::$debug=1;
+
 Config::$config=[
-    // PHP SDK 社区试用API，使用时，建议改为自己的API KEY
-    "apiKey"=>"56094d83-51c5-4cb5-b488-c7db65c2fa05",
-    "apiSecret"=>"8757143A3AD847E162FDB088615A92A0",
-    "passphrase"=>"123456",
+    // PHP SDK 填写个人的key
+//    "apiKey"=>"",
+//    "apiSecret"=>"",
+//    "passphrase"=>"",
 ];
